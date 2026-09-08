e9cb8cf001ea8b5d4aafd9f0ae7bae386975fb79
haiht1 <haiht1@cnv.vn>
2026-09-08 16:05:59 +0700

    style: ngôi sao bỏ viền tròn, chỉ còn hình sao nhấp nháy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V = 'tdtd-v9';
+const V = 'tdtd-v10';
 const SHELL = ['./', 'index.html', 'assets/app.css', 'assets/app.js', 'data/cards.json',
                'manifest.webmanifest', 'icons/icon-192.png', 'icons/icon-512.png'];
 self.addEventListener('install', e => {
